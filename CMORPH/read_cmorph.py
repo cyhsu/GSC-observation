c088b31620355fe017731bc3a263788cb05f4a61
C.Y. Hsu <chsu1@tamu.edu>
2019-07-05 15:14:55 -0500

    Update read_cmorph.py

--- CMORPH/read_cmorph.py
+++ CMORPH/read_cmorph.py
@@ -42,3 +42,16 @@ df.to_netcdf(outnc,encoding={'time':{'units': 'days since 1990-01-01'}},
 				 unlimited_dims={'time':True})
 	
 
+	
+	
+	
+##-- test plot:
+#fig, axes = plt.subplots(ncols=1,nrows=1,figsize=(10,8), subplot_kw=dict(projection=ccrs.PlateCarree()))             
+#axes.set_extent([-100+360,-77+360,18,35])  
+#axes.coastlines(resolution='50m',lw=1.0,color='white')  
+#axes.add_feature(cf.OCEAN.with_scale('50m'),lw=1.0,edgecolor='white')  
+#axes.add_feature(cf.LAND.with_scale('50m'),lw=1.0,edgecolor='white')    
+#axes.pcolormesh(df.lon.data,df.lat.data,df.sel(time='2017-08-30').cmorph.data,vmin=0,vmax=300,cmap=plt.cm.jet)
+
+##-- Error --: Need to check the error message... (2019-07-05)
+##df.sel(time='2017-08-30').cmorph.plot.pcolormesh(ax=axes,vmin=0,vmax=350,cmap=plt.cm.jet) 
